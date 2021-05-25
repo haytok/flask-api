5376617229d860c4f231d98cad416bec8c350af5
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-05-25 17:07:56 +0900

    create main.py of http-request

--- http-request/main.py
+++ http-request/main.py
@@ -1,7 +1,9 @@
-import requests
-import json
-import sys
 import argparse
+import json
+
+import requests
+from dotenv import dotenv_values
+
 
 class Main(object):
     def __init__(self, email, password, username, base_url):
@@ -70,25 +72,26 @@ class Main(object):
         )
         return login_user_response.json()
 
-
     def create_article(self):
         pass
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     # コマンドライン引数から値を受け取る
     parser = argparse.ArgumentParser()
     parser.add_argument('-e', '--environment', help='set environment.')
     args = parser.parse_args()
-    print(args.environment)
 
     # テストする際に定義する必要がある。
     email = 'test_1@mail.com'
     password = 'password'
     username = 'test_1'
 
-    dev_base_url = 'http://127.0.0.1:5000'
-    prod_base_url = 'https://boiling-wildwood-20063.herokuapp.com'
-    base_url = dev_base_url if args.environment == 'dev' else prod_base_url
+    # 環境変数を読み出す
+    configs = dotenv_values('.env')
+    base_url = configs['DEV_BASE_URL'] if args.environment == 'dev' else configs['PROD_BASE_URL']
 
+    print(base_url)
+    
     main = Main(email, password, username, base_url)
     main.create_user()
