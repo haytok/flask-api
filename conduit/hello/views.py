94790a0488b7d4e2b2f631903800d088c95c23c4
dilmnqvovpnmlib <simplelpmis6@gmail.com>
2021-08-24 04:06:33 +0900

    update endpoint of hello

--- conduit/hello/views.py
+++ conduit/hello/views.py
@@ -1,7 +1,19 @@
+import time
+
 from flask import Blueprint, jsonify
 
 blueprint = Blueprint('hello', __name__)
 
 @blueprint.route('/api/hello', methods=('GET', ))
 def get_hello():
-    return jsonify({'value': 'Hello World'})
+    query = request.args
+
+    sleep_time = int(query.get('sleep_time', '0'))
+    if sleep_time > 0:
+        time.sleep(sleep_time)
+
+    res = {
+        'value': 'Hello World',
+        'sleep_time': sleep_time,
+    }
+    return jsonify(res)
